ba5dc87e51a2f18abc95de33c86420fe55780efe
Gisela Schierff <56985996+GiselaSchierff@users.noreply.github.com>
2020-08-06 09:54:32 -0300

    Add files via upload

---
+++ TP1ejercicio16bis.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 19:50:13 2020
+
+@author: Gisela Schierff
+"""
+'''
+Ejercicio 16: Crear un programa que cree un diccionario vacío y lo vaya llenado con información sobre una persona (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.) que se le pida al usuario. Cada vez que se añada un nuevo dato debe imprimirse el contenido del diccionario.
+'''
+diccionarioALlenar = {}
+respuesta = "s"
+
+while respuesta = "s"
+     clave = input("¿Qué tipo de información va a ingresar?")"
+     valor = input(f"{clave} : ")
+     diccionarioALlenar[clave] = valor
+     print(diccionarioALlenar)
+     respuesta = input(''' Si desea ingresar más información presione "s", 
+                       en caso contrario presione cualquier tecla''')
+                       
+                       
